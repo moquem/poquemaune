da95386648e2f9038605e0f7320c677a3b473203
Tanguy Bozec <bozec.tanguy@gmail.com>
2022-02-04 16:49:38 +0100

    nouveau hello world de fenetre swing

--- src/main/scala/main_window.scala
+++ src/main/scala/main_window.scala
@@ -1,9 +1,19 @@
 import swing._
-object HelloWorld extends SimpleSwingApplication {
-  def top = new MainFrame {
-    title = "Hello, World!"
-    contents = new Button {
-      text = "Click Me!"
+
+import scala.swing._
+
+class UI extends MainFrame {
+  title = "GUI Program #1"
+  preferredSize = new Dimension(320, 240)
+  contents = new Label("Here is the contents!")
+}
+
+object GuiProgramOne {
+  def main(args: Array[String]) {
+    val ui = new UI
+    ui.visible = true
+    while (true){
     }
+    println("End of main function")
   }
 }
