a75f422a5abe14684689719fb4208880894fe6fe
Tanguy Bozec <bozec.tanguy@gmail.com>
2022-02-04 16:31:11 +0100

    basic folder structure with basic window

---
+++ src/main/scala/main_window.scala
@@ -0,0 +1,9 @@
+import swing._
+object HelloWorld extends SimpleSwingApplication {
+  def top = new MainFrame {
+    title = "Hello, World!"
+    contents = new Button {
+      text = "Click Me!"
+    }
+  }
+}
