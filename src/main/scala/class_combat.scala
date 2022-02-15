8aa5b198d6494f1ef7b39a78f3e77c73bb51168e
Tanguy Bozec <bozec.tanguy@gmail.com>
2022-02-15 23:56:08 +0100

    bugfix team variable parameters

--- src/main/scala/class_combat.scala
+++ src/main/scala/class_combat.scala
@@ -3,7 +3,11 @@ import java.awt.event._
 
 
 class Fight (team1:Team, team2:Team) {
-
+  
+    var team_1 = new Team 
+    team_1 = team1
+    var team_2 = new Team
+    team_2 = team2
     var current_pok_1 = team1.team(0)
     var current_pok_2 = team2.team(0)
 
