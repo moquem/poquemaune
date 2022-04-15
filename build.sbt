4b1c19fb052c68fff3c3f17b94897ae48bc31b1d
Vincent Lafeychine <vincent.lafeychine@gmail.com>
2022-04-15 22:22:03 +0200

    magie cryptique du V

--- build.sbt
+++ build.sbt
@@ -1,9 +1,7 @@
-val swing = "org.scala-lang.modules" %% "scala-swing" % "3.0.0"
-
+name := "poquemaune"
 scalaVersion := "3.1.1"
 
-lazy val root = (project in file(".")).settings(
-    name := "My Project",
-    libraryDependencies += swing,
-    Compile / unmanagedJars += file("lib/tablelayout.jar")
-)
+enablePlugins(ScalaNativePlugin)
+
+nativeMode := "debug" // "release-full"
+nativeLTO := "none" // "thin"
---
+++ src/main/scala/App.scala
@@ -0,0 +1,24 @@
+import sfml.graphics.*
+import sfml.window.*
+
+@main def main =
+  val window = RenderWindow(VideoMode(800, 600, 32), "SFML", Window.WindowStyle.DefaultStyle)
+  val texture = Texture("src/main/resources/player_icon.jpeg")
+  val sprite = Sprite(texture)
+  val font = Font("src/main/resources/test_font.ttf")
+  val text = Text()
+  text.string = "babebibobu"
+  text.font = font
+  while window.isOpen() do
+    for event <- window.pollEvent() do
+      println(event)
+      event match
+        case Event.Closed() => window.close()
+        case _ => ()
+    
+    window.clear(Color.Magenta())
+    sprite.position = (100, 200)
+    window.draw(sprite)
+    text.position = (200, 400)
+    window.draw(text)
+    window.display()
