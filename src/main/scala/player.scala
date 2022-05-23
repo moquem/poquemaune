cefe655213657090ae63e1bd4c1b1459f3ab0307
moquem <maena.quemener@yahoo.com>
2022-05-23 22:48:01 +0200

    add info

--- src/main/scala/player.scala
+++ src/main/scala/player.scala
@@ -5,11 +5,24 @@ import sfml.window.*
 object Player {
   
   val inventory = new Inventory()
-  var ownedPokList = Array[Pokemon](Pokemon("src/main/resources/pokemons/bestPokemon.txt"))
-  for (i <- 0 to 35) {
+  var ownedPokList = Array[Pokemon](Pokemon("src/main/resources/pokemons/Jean.txt"))
+  ownedPokList = ownedPokList ++ Array(Pokemon("src/main/resources/pokemons/Arthur.txt"))
+  ownedPokList = ownedPokList ++ Array(Pokemon("src/main/resources/pokemons/Antonin.txt"))
+  ownedPokList = ownedPokList ++ Array(Pokemon("src/main/resources/pokemons/Augustin.txt"))
+  ownedPokList = ownedPokList ++ Array(Pokemon("src/main/resources/pokemons/Pauline.txt"))
+  ownedPokList = ownedPokList ++ Array(Pokemon("src/main/resources/pokemons/Leopold.txt"))
+  ownedPokList = ownedPokList ++ Array(Pokemon("src/main/resources/pokemons/Tanguy.txt"))
+  ownedPokList = ownedPokList ++ Array(Pokemon("src/main/resources/pokemons/Erwann.txt"))
+  ownedPokList = ownedPokList ++ Array(Pokemon("src/main/resources/pokemons/Esteban.txt"))
+  ownedPokList = ownedPokList ++ Array(Pokemon("src/main/resources/pokemons/Hubert.txt"))
+  ownedPokList = ownedPokList ++ Array(Pokemon("src/main/resources/pokemons/Ewen.txt"))
+  ownedPokList = ownedPokList ++ Array(Pokemon("src/main/resources/pokemons/Mina.txt"))
+  ownedPokList = ownedPokList ++ Array(Pokemon("src/main/resources/pokemons/Maena.txt"))
+  ownedPokList = ownedPokList ++ Array(Pokemon("src/main/resources/pokemons/Vincent.txt"))
+  ownedPokList = ownedPokList ++ Array(Pokemon("src/main/resources/pokemons/Valentin.txt"))
+  for (i <- 0 to 22) {
     ownedPokList = ownedPokList ++ Array(Pokemon("src/main/resources/pokemons/uselessPok.txt"))
   }
-  ownedPokList = ownedPokList ++ Array(Pokemon("src/main/resources/pokemons/testPokemon.txt"))
 
   inventory.ownedPokList = ownedPokList
 
